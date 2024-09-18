987c9fbba41dcd6b2af411d34f44706bfd0f415d
Sonali Chauhan <sonali@Sonalis-Mac-mini.local>
2024-09-18 14:57:39 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -5,114 +5,83 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Button,
+  Keyboard,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App(): React.JSX.Element {
+  const [contact, setContact] = useState("+91");
+  const [code, setCode] = useState('');
+  const [verificationId, setVerificationId] = useState(null);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    console.log("Run....")
+  },undefined)
+
+  const handleContact = (text: string) => {
+    setContact(text)
+  }
+
+  const onSubmitEditing = () => {
+    Keyboard.dismiss();
+  }
+
+  const generateOtp = async () => {
+    try {
+      const confirmation: any = await auth().signInWithPhoneNumber(contact);
+      setVerificationId(confirmation.verificationId);
+    } catch (err) {
+      console.log("In catch block =====>", err)
+    }
+  }
+
+  const confirmOTP = async () => {
+    try {
+      const credential = auth.PhoneAuthProvider.credential(verificationId, code);
+      await auth().signInWithCredential(credential);
+      console.log('Phone number verified!');
+    } catch (error) {
+      console.error('Error verifying OTP:', error);
+    }
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TextInput
+        value={contact}
+        onChangeText={(text) => handleContact(text)}
+        style={{ borderColor: "green", width: "80%", borderWidth: 1 }}
+        maxLength={13}
+        keyboardType={"numeric"}
+        onSubmitEditing={onSubmitEditing}
+      />
+      <Button title={"Send OTP"} onPress={generateOtp} />
+      {verificationId && (
+        <>
+          <TextInput
+            placeholder="OTP"
+            value={code}
+            onChangeText={(text) => { setCode(text) }}
+            keyboardType="number-pad"
+          />
+          <Button title="Confirm OTP" onPress={confirmOTP} />
+        </>
+      )}
+      <Text style={{ color: "black", fontSize: 20 }}>{code}</Text>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
